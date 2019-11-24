266dcef18fcfca12b8192021e39737df2518a2fb
Sebastien Roy <seb@delphix.com>
2019-11-24 15:15:37 -0500

    connstat suna column doesn't seem to represent send+unacknowledged data #17 connstat should gracefully handle keyboard interrupt #15 connstat command can't handle running on an older kernel #18 would like to display TCP receive queue size with connstat #20
    The main bug being fixed (#17) is caused by the caller of
    record_ipv4_route() passing in the last two arguments in the wrong
    order.
    
    In addition to this simple fix, in order to prevent a similar error in
    the future, record_ipv4_route() (which is renamed to record_ipv4_conn())
    now takes a pointer to a pameter structure instead of a large number of
    arguments.
    
    This change also applies LLVM code style to the kernel module source,
    and adds a GitHub Action to check python lint using flake8.

--- module/src/connstat.c
+++ module/src/connstat.c
@@ -11,134 +11,133 @@
  */
 
 #include <net/tcp.h>
+#include <linux/inet.h>
 #include <linux/proc_fs.h>
 
-typedef char ipv4_ip[15];
-
-#define GET_OCTET0(hip) (hip & 0xFF)
-#define GET_OCTET1(hip) ((hip>>8) & 0xFF)
-#define GET_OCTET2(hip) ((hip>>16) & 0xFF)
-#define GET_OCTET3(hip) (hip>>24)
-
-static char *tcp_state_strings[] = {
-    "NONE",
-    "ESTABLISHED",
-    "SYN_SENT",
-    "SYN_RECEIVED",
-    "FIN_WAIT1",
-    "FIN_WAIT2",
-    "TIME_WAIT",
-    "CLOSED",
-    "CLOSE_WAIT",
-    "LAST_ACK",
-    "LISTEN",
-    "CLOSING",
-    "NEW_SYN_RECV",
-    "MAX_STATES"
+struct connstat_data {
+	__be32 laddr;
+	__be32 raddr;
+	__u16 lport;
+	__u16 rport;
+	int state;
+	u32 cwnd;
+	u32 rwnd;
+	u32 swnd;
+	u32 mss;
+	u32 insegs;
+	u32 outsegs;
+	u32 retranssegs;
+	u32 rto;
+	u32 rtt;
+	u32 suna;
+	u32 unsent;
+	u32 rxqueue;
+	u64 inbytes;
+	u64 outbytes;
 };
 
-/* Convert hex ip to ipv4(XXX.XXX.XXX.XXX) */
-void hex_to_ipv4_ip(__be32 hip, ipv4_ip v4ip)
+#define GET_OCTET0(hip) (hip & 0xFF)
+#define GET_OCTET1(hip) ((hip >> 8) & 0xFF)
+#define GET_OCTET2(hip) ((hip >> 16) & 0xFF)
+#define GET_OCTET3(hip) (hip >> 24)
+
+static char *tcp_state_strings[] = {
+	"NONE",	     "ESTABLISHED", "SYN_SENT",	    "SYN_RECEIVED", "FIN_WAIT1",
+	"FIN_WAIT2", "TIME_WAIT",   "CLOSED",	    "CLOSE_WAIT",   "LAST_ACK",
+	"LISTEN",    "CLOSING",	    "NEW_SYN_RECV", "MAX_STATES"
+};
+
+/* Print an IPv4 address into the provided string and return that string. */
+static char *ipv4_ntop(__be32 addr, char *addrstr)
 {
-	sprintf(v4ip,"%d.%d.%d.%d", GET_OCTET0(hip), GET_OCTET1(hip),
-	    GET_OCTET2(hip), GET_OCTET3(hip));
+	snprintf(addrstr, INET_ADDRSTRLEN, "%d.%d.%d.%d", GET_OCTET0(addr),
+		 GET_OCTET1(addr), GET_OCTET2(addr), GET_OCTET3(addr));
+	return addrstr;
 }
 
-static void record_ipv4_route(struct seq_file *f, __be32 src,
-    __u16 srcp, __be32 dest, __u16 destp, int state, u32 cwnd, u32 rwnd,
-    u32 swnd, u32 mss, u64 inbytes, u32 insegs, u64 outbytes, u32 outsegs,
-    u32 retranssegs, u32 rto, u32 rtt, u32 suna, u32 unsent)
+static void record_ipv4_conn(struct seq_file *f, struct connstat_data *data)
 {
-	ipv4_ip laddr, raddr;
+	char laddr[INET_ADDRSTRLEN], raddr[INET_ADDRSTRLEN];
 
-	hex_to_ipv4_ip(src, laddr);
-	hex_to_ipv4_ip(dest, raddr);
-
-        seq_printf(f, "%s,%u,%s,%u,%s,%llu,%u,%llu,%u,%u,"
-	    "%u,%u,%u,%u,%u,%u,%u,%u",
-	    laddr, srcp, raddr, destp, tcp_state_strings[state],
-	    inbytes, insegs, outbytes, outsegs, retranssegs,
-	    suna, unsent, swnd, cwnd, rwnd, mss, rto, rtt);
+	seq_printf(f,
+		   "%s,%u,%s,%u,%s,%llu,%u,%llu,%u,%u,"
+		   "%u,%u,%u,%u,%u,%u,%u,%u,%u",
+		   ipv4_ntop(data->laddr, laddr), data->lport,
+		   ipv4_ntop(data->raddr, raddr), data->rport,
+		   tcp_state_strings[data->state], data->inbytes, data->insegs,
+		   data->outbytes, data->outsegs, data->retranssegs, data->suna,
+		   data->unsent, data->swnd, data->cwnd, data->rwnd, data->mss,
+		   data->rto, data->rtt, data->rxqueue);
 }
 
-
-static void get_openreq4(const struct request_sock *req,
-                         struct seq_file *f)
+static void get_openreq4(const struct request_sock *req, struct seq_file *f)
 {
-        const struct inet_request_sock *ireq = inet_rsk(req);
+	const struct inet_request_sock *ireq = inet_rsk(req);
+	struct connstat_data data = { 0 };
 
-	record_ipv4_route(f, ireq->ir_loc_addr, ireq->ir_num,
-	    ireq->ir_rmt_addr, ntohs(ireq->ir_rmt_port), TCP_SYN_RECV, 0, 0,
-	    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+	data.laddr = ireq->ir_loc_addr;
+	data.raddr = ireq->ir_rmt_addr;
+	data.lport = ireq->ir_num;
+	data.rport = ntohs(ireq->ir_rmt_port);
+	data.state = TCP_SYN_RECV;
 
+	record_ipv4_conn(f, &data);
 }
 
 static void get_tcp4_sock(struct sock *sk, struct seq_file *f)
 {
-        int timer_active;
-        unsigned long timer_expires;
-        const struct tcp_sock *tp = tcp_sk(sk);
-        const struct inet_connection_sock *icsk = inet_csk(sk);
-        const struct inet_sock *inet = inet_sk(sk);
-        __be32 dest = inet->inet_daddr;
-        __be32 src = inet->inet_rcv_saddr;
-        __u16 destp = ntohs(inet->inet_dport);
-        __u16 srcp = ntohs(inet->inet_sport);
-        int rx_queue;
-        int state;
+	const struct tcp_sock *tp = tcp_sk(sk);
+	const struct inet_connection_sock *icsk = inet_csk(sk);
+	const struct inet_sock *inet = inet_sk(sk);
+	struct connstat_data data = { 0 };
 
-        if (icsk->icsk_pending == ICSK_TIME_RETRANS ||
-            icsk->icsk_pending == ICSK_TIME_REO_TIMEOUT ||
-            icsk->icsk_pending == ICSK_TIME_LOSS_PROBE) {
-                timer_active    = 1;
-                timer_expires   = icsk->icsk_timeout;
-        } else if (icsk->icsk_pending == ICSK_TIME_PROBE0) {
-                timer_active    = 4;
-                timer_expires   = icsk->icsk_timeout;
-        } else if (timer_pending(&sk->sk_timer)) {
-                timer_active    = 2;
-                timer_expires   = sk->sk_timer.expires;
-        } else {
-                timer_active    = 0;
-                timer_expires = jiffies;
-        }
-
+	data.laddr = inet->inet_rcv_saddr;
+	data.raddr = inet->inet_daddr;
+	data.lport = ntohs(inet->inet_sport);
+	data.rport = ntohs(inet->inet_dport);
 #if (KERNEL_CENTEVERSION > 415)
-	state = inet_sk_state_load(sk);
+	data.state = inet_sk_state_load(sk);
 #else
-	state = sk_state_load(sk);
+	data.state = sk_state_load(sk);
 #endif
+	data.cwnd = tp->snd_cwnd * tp->mss_cache;
+	data.rwnd = tp->rcv_wnd;
+	data.swnd = tp->snd_wnd;
+	data.mss = tp->advmss;
+	data.insegs = tp->segs_in;
+	data.outsegs = tp->segs_out;
+	data.retranssegs = tp->total_retrans;
+	data.rto = jiffies_to_clock_t(icsk->icsk_rto);
+	data.rtt = tp->srtt_us;
+	data.suna = tp->snd_nxt - tp->snd_una;
+	data.unsent = tp->write_seq - tp->snd_nxt;
+	data.inbytes = tp->bytes_received;
+	data.outbytes = tp->bytes_acked;
 
-        if (state == TCP_LISTEN)
-                rx_queue = sk->sk_ack_backlog;
-        else
-                /* Because we don't lock the socket,
-                 * we might find a transient negative value.
-                 */
-                rx_queue = max_t(int, tp->rcv_nxt - tp->copied_seq, 0);
+	if (data.state == TCP_LISTEN) {
+		data.rxqueue = sk->sk_ack_backlog;
+	} else {
+		/* Because we don't lock the socket,
+		 * we might find a transient negative value.
+		 */
+		data.rxqueue = max_t(int, tp->rcv_nxt - tp->copied_seq, 0);
+	}
 
-	record_ipv4_route(f, src, srcp, dest, destp, state,
-	    tp->snd_cwnd * tp->mss_cache, tp->rcv_wnd, tp->snd_wnd,
-	    tp->advmss, tp->bytes_received, tp->segs_in, tp->bytes_acked,
-	    tp->segs_out, tp->total_retrans, jiffies_to_clock_t(icsk->icsk_rto),
-	    tp->srtt_us, tp->write_seq - tp->pushed_seq,
-	    tp->pushed_seq - tp->snd_una);
+	record_ipv4_conn(f, &data);
 }
 
-
 static void get_timewait4_sock(const struct inet_timewait_sock *tw,
-                               struct seq_file *f)
+			       struct seq_file *f)
 {
-        __be32 dest, src;
-        __u16 destp, srcp;
+	struct connstat_data data = { 0 };
 
-        dest  = tw->tw_daddr;
-        src   = tw->tw_rcv_saddr;
-        destp = ntohs(tw->tw_dport);
-        srcp  = ntohs(tw->tw_sport);
+	data.laddr = tw->tw_rcv_saddr;
+	data.raddr = tw->tw_daddr;
+	data.lport = ntohs(tw->tw_sport);
+	data.rport = ntohs(tw->tw_dport);
+	data.state = tw->tw_substate;
 
-	record_ipv4_route(f, src, srcp, dest, destp, tw->tw_substate, 0, 0,
-	    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+	record_ipv4_conn(f, &data);
 }
 
 #define TMPSZ 150
@@ -147,29 +146,29 @@ static int connstat_seq_show(struct seq_file *seq, void *v)
 {
 	struct sock *sk = v;
 
-        seq_setwidth(seq, TMPSZ - 1);
-        if (v == SEQ_START_TOKEN) {
-		seq_puts(seq,
-		    "laddr,"
-		    "lport,"
-		    "raddr,"
-		    "rport,"
-		    "state,"
-		    "inbytes,"
-		    "insegs,"
-		    "outbytes,"
-		    "outsegs,"
-		    "retranssegs,"
-		    "suna,"
-		    "unsent,"
-		    "swnd,"
-		    "cwnd,"
-		    "rwnd,"
-		    "mss,"
-		    "rto,"
-		    "rtt");
-                goto out;
-        }
+	seq_setwidth(seq, TMPSZ - 1);
+	if (v == SEQ_START_TOKEN) {
+		seq_puts(seq, "laddr,"
+			      "lport,"
+			      "raddr,"
+			      "rport,"
+			      "state,"
+			      "inbytes,"
+			      "insegs,"
+			      "outbytes,"
+			      "outsegs,"
+			      "retranssegs,"
+			      "suna,"
+			      "unsent,"
+			      "swnd,"
+			      "cwnd,"
+			      "rwnd,"
+			      "mss,"
+			      "rto,"
+			      "rtt,"
+			      "rxqueue");
+		goto out;
+	}
 
 	if (sk->sk_state == TCP_TIME_WAIT)
 		get_timewait4_sock(v, seq);
@@ -178,8 +177,8 @@ static int connstat_seq_show(struct seq_file *seq, void *v)
 	else
 		get_tcp4_sock(v, seq);
 out:
-        seq_pad(seq, '\n');
-        return 0;
+	seq_pad(seq, '\n');
+	return 0;
 }
 
 /*
@@ -190,51 +189,52 @@ out:
 #if (KERNEL_CENTEVERSION > 415)
 
 static const struct seq_operations connstat_seq_ops = {
-        .show           = connstat_seq_show,
-        .start          = tcp_seq_start,
-        .next           = tcp_seq_next,
-        .stop           = tcp_seq_stop,
+	.show = connstat_seq_show,
+	.start = tcp_seq_start,
+	.next = tcp_seq_next,
+	.stop = tcp_seq_stop,
 };
 
 static struct tcp_seq_afinfo connstat_seq_afinfo = {
-        .family         = AF_INET,
+	.family = AF_INET,
 };
 
 static int __net_init connstat_proc_init_net(struct net *net)
 {
-        if (!proc_create_net_data("stats_tcp", 0444, net->proc_net, &connstat_seq_ops,
-                        sizeof(struct tcp_iter_state), &connstat_seq_afinfo))
-                return -ENOMEM;
-        return 0;
+	if (!proc_create_net_data(
+		    "stats_tcp", 0444, net->proc_net, &connstat_seq_ops,
+		    sizeof(struct tcp_iter_state), &connstat_seq_afinfo))
+		return -ENOMEM;
+	return 0;
 }
 
 static void __net_exit connstat_proc_exit_net(struct net *net)
 {
-        remove_proc_entry("stats_tcp", net->proc_net);
+	remove_proc_entry("stats_tcp", net->proc_net);
 }
 
 #else
 
 static const struct file_operations connstat_seq_fops = {
-        .owner   = THIS_MODULE,
-        .open    = tcp_seq_open,
-        .read    = seq_read,
-        .llseek  = seq_lseek,
-        .release = seq_release_net
+	.owner = THIS_MODULE,
+	.open = tcp_seq_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = seq_release_net
 };
 
 static struct tcp_seq_afinfo connstat_seq_afinfo = {
-        .name           = "stats_tcp",
+	.name           = "stats_tcp",
 	.family         = AF_INET,
-        .seq_fops       = &connstat_seq_fops,
-        .seq_ops        = {
-                .show           = connstat_seq_show,
+	.seq_fops       = &connstat_seq_fops,
+	.seq_ops        = {
+		.show           = connstat_seq_show,
         },
 };
 
 static int __net_init connstat_proc_init_net(struct net *net)
 {
-        return tcp_proc_register(net, &connstat_seq_afinfo);
+	return tcp_proc_register(net, &connstat_seq_afinfo);
 }
 
 static void __net_exit connstat_proc_exit_net(struct net *net)
@@ -245,10 +245,9 @@ static void __net_exit connstat_proc_exit_net(struct net *net)
 #endif
 
 static struct pernet_operations connstat_net_ops = {
-        .init = connstat_proc_init_net,
-        .exit = connstat_proc_exit_net,
+	.init = connstat_proc_init_net,
+	.exit = connstat_proc_exit_net,
 };
-
 
 int connstat_init_module(void)
 {
